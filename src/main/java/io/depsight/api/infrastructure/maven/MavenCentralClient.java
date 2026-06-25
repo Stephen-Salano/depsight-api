2ba28052852627c74021355e71e2ed9d715102b8
Salano <xirlanostephen@gmail.com>
2026-06-25 16:44:08 +0300

    core(MavenClient): Added a Reactive client for calls to MavenCentral APi
    - Created a custom client for MavenCentralClient calls to the maven main
    repository for parent bom fetches using the existing WebClientConfig
    builer.
    - Added custom ExternalApiError exceptions

--- src/main/java/io/depsight/api/infrastructure/maven/MavenCentralClient.java
+++ src/main/java/io/depsight/api/infrastructure/maven/MavenCentralClient.java
@@ -1,33 +1,47 @@
 package io.depsight.api.infrastructure.maven;
 
-import org.springframework.stereotype.Component;
+import io.depsight.api.common.exception.ExternalApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-@Component
+@Service
+@Slf4j
 public class MavenCentralClient {
-  private static final String BASE_URL = "https://repo1.maven.org/maven2/";
-  // private final HttpClient client = HttpClient.newBuilder
-  // public String fetchPom(MavenCooridinates cooridinates) {
-  //   String url = buildUrl(cooridinates);
-  //   String fetchedPom = httpGet(url);
-  // }
-  //
-  // private String httpGet(String url) {
-  //
-  // }
-  //
-  // private String buildUrl(MavenCooridinates cooridinates) {
-  //   String groupPath = cooridinates.parentGroupId().replace(".", "/");
-  //
-  //   return BASE_URL
-  //       + groupPath
-  //       + "/"
-  //       + cooridinates.parentArtifactId()
-  //       + "/"
-  //       + cooridinates.parentVersion()
-  //       + "/"
-  //       + cooridinates.parentArtifactId()
-  //       + "-"
-  //       + cooridinates.parentVersion()
-  //       + ".pom";
-  // }
+
+  private final WebClient client;
+
+  public MavenCentralClient(WebClient.Builder builder) {
+    this.client = builder.baseUrl("https://repo1.maven.org/maven2/").build();
+  }
+
+  public Mono<String> fetchPom(String groupId, String artifactId, String version) {
+    return client
+        .get()
+        .uri(buildUrl(groupId, artifactId, version))
+        .retrieve()
+        .onStatus(status -> status.value() == 404, resp -> Mono.empty())
+        .onStatus(
+            status -> status.isError(),
+            resp -> Mono.error(new ExternalApiException("Something went wrong while fetching")))
+        .bodyToMono(String.class)
+        .onErrorResume(ExternalApiException.class, e -> Mono.error(e));
+  }
+
+  private String buildUrl(String groupId, String artifactId, String version) {
+
+    String groupPath = groupId.replace(".", "/");
+    // Creating the url from the MavenCooridinates
+    return groupPath //  org.springframwork.boot => org/springframwork/boot
+        + "/" // org/springframwork/boot/
+        + artifactId // org/springframwork/boot/spring-boot-starter-parent
+        + "/" // org/springframwork/boot/spring-boot-starter-parent/
+        + version // org/springframwork/boot/spring-boot-starter-parent/4.0.6
+        + "/" // org/springframwork/boot/spring-boot-starter-parent/4.0.6/
+        + artifactId // org/springframwork/boot/spring-boot-starter-parent/4.0.6/spring-boot-starter-parent
+        + "-" // org/springframwork/boot/spring-boot-starter-parent/4.0.6/spring-boot-starter-parent-
+        + version // org/springframwork/boot/spring-boot-starter-parent/4.0.6/spring-boot-starter-parent-4.0.6
+        + ".pom"; // org/springframwork/boot/spring-boot-starter-parent/4.0.6/spring-boot-starter-parent-4.0.6.pom (the full link)
+  }
 }
